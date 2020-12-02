c4976fd7adc1c9d261d5dc25554341061ca5b303
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-12-02 15:18:14 -0600

    se agrego la inclucion a empleadosForm, la parte de empleadosCreate y empleadosEdit

--- resources/views/empleados/empleadosForm.blade.php
+++ resources/views/empleados/empleadosForm.blade.php
@@ -4,38 +4,46 @@
 
 
 <label for="Nombre">{{ 'Nombre' }}</label>
-<input type="text" name="Nombre" id="Nombre" value="">
+<input type="text" name="Nombre" id="Nombre"
+value="{{ isset($empleado->Nombre)?$empleado->Nombre : '' }}">
 <br>
 
 <label for="ApellidoPaterno">{{ 'Apellido Paterno' }}</label>
-<input type="text" name="ApellidoPaterno" id="ApellidoPaterno" value="">
+<input type="text" name="ApellidoPaterno" id="ApellidoPaterno"
+value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno : '' }}">
 <br>
 
 <label for="ApellidoMaterno">{{ 'Apellido Materno' }}</label>
-<input type="text" name="ApellidoMaterno" id="ApellidoMaterno" value="">
+<input type="text" name="ApellidoMaterno" id="ApellidoMaterno"
+value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno : '' }}">
 <br>
 
 <label for="Direccion">{{ 'Direccion' }}</label>
-<input type="text" name="Direccion" id="Direccion" value="">
+<input type="text" name="Direccion" id="Direccion"
+value="{{ isset($empleado->Direccion)?$empleado->Direccion : '' }}">
 <br>
 
 <label for="Municipio">{{ 'Municipio' }}</label>
-<input type="text" name="Municipio" id="Municipio" value="">
+<input type="text" name="Municipio" id="Municipio"
+value="{{ isset($empleado->Municipio)?$empleado->Municipio : '' }}">
 <br>
 
 <label for="Telefono">{{ 'Telefono' }}</label>
-<input type="text" name="Telefono" id="Telefono" value="">
+<input type="text" name="Telefono" id="Telefono"
+value="{{ isset($empleado->Telefono)?$empleado->Telefono : '' }}">
 <br>
 
 <label for="Correo">{{ 'Correo' }}</label>
-<input type="email" name="Correo" id="Correo" value="">
+<input type="email" name="Correo" id="Correo"
+value="{{ isset($empleado->Correo)?$empleado->Correo : '' }}">
 <br>
 
 <label for="Edad">{{ 'Edad' }}</label>
-<input type="text" name="Edad" id="Edad" value="">
+<input type="text" name="Edad" id="Edad"
+value="{{ isset($empleado->Edad)?$empleado->Edad : '' }}">
 <br>
 
-<input type="submit" value="Agregar">
+<input type="submit" value="{{ $Modo =='crear' ?  'Agregar' : 'Modificar'}}">
 
-<br>
+
 <a href="{{ url('empleados') }}">Regresar</a>
