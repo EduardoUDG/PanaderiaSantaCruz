e29f3de5c4cd0f7f4e3a2b385c3f2abdd5d67f2e
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-12-02 22:56:38 -0600

    se termino la validacion del formulario con los mensajes antes de agregar el empleado

--- resources/views/empleados/empleadosForm.blade.php
+++ resources/views/empleados/empleadosForm.blade.php
@@ -10,38 +10,45 @@ value="{{ isset($empleado->Nombre)?$empleado->Nombre : old('Nombre') }}">
 <br>
 
 <label for="ApellidoPaterno">{{ 'Apellido Paterno' }}</label>
-<input type="text" name="ApellidoPaterno" id="ApellidoPaterno"
-value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno : '' }}">
+<input type="text" name="ApellidoPaterno" id="ApellidoPaterno" class="{{ $errors->has('ApellidoPaterno')?'is-invalid':'' }}"
+value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno : old('ApellidoPaterno') }}">
+{!! $errors->first('ApellidoPaterno','<div class="invalid-feedback">:message</div>') !!}
 <br>
 
 <label for="ApellidoMaterno">{{ 'Apellido Materno' }}</label>
-<input type="text" name="ApellidoMaterno" id="ApellidoMaterno"
-value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno : '' }}">
+<input type="text" name="ApellidoMaterno" id="ApellidoMaterno" class="{{ $errors->has('ApellidoMaterno')?'is-invalid':'' }}"
+value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno : old('ApellidoMaterno') }}">
+{!! $errors->first('ApellidoMaterno','<div class="invalid-feedback">:message</div>') !!}
 <br>
 
 <label for="Direccion">{{ 'Direccion' }}</label>
-<input type="text" name="Direccion" id="Direccion"
-value="{{ isset($empleado->Direccion)?$empleado->Direccion : '' }}">
+<input type="text" name="Direccion" id="Direccion" class="{{ $errors->has('Direccion')?'is-invalid':'' }}"
+value="{{ isset($empleado->Direccion)?$empleado->Direccion : old('Direccion') }}">
+{!! $errors->first('Direccion','<div class="invalid-feedback">:message</div>') !!}
 <br>
 
 <label for="Municipio">{{ 'Municipio' }}</label>
-<input type="text" name="Municipio" id="Municipio"
-value="{{ isset($empleado->Municipio)?$empleado->Municipio : '' }}">
+<input type="text" name="Municipio" id="Municipio" class="{{ $errors->has('Municipio')?'is-invalid':'' }}"
+value="{{ isset($empleado->Municipio)?$empleado->Municipio : old('Municipio') }}">
+{!! $errors->first('Municipio','<div class="invalid-feedback">:message</div>') !!}
 <br>
 
 <label for="Telefono">{{ 'Telefono' }}</label>
-<input type="text" name="Telefono" id="Telefono"
-value="{{ isset($empleado->Telefono)?$empleado->Telefono : '' }}">
+<input type="text" name="Telefono" id="Telefono" class="{{ $errors->has('Telefono')?'is-invalid':'' }}"
+value="{{ isset($empleado->Telefono)?$empleado->Telefono : old('Telefono') }}">
+{!! $errors->first('Telefono','<div class="invalid-feedback">:message</div>') !!}
 <br>
 
 <label for="Correo">{{ 'Correo' }}</label>
-<input type="email" name="Correo" id="Correo"
-value="{{ isset($empleado->Correo)?$empleado->Correo : '' }}">
+<input type="email" name="Correo" id="Correo" class="{{ $errors->has('Correo')?'is-invalid':'' }}"
+value="{{ isset($empleado->Correo)?$empleado->Correo : old('Correo') }}">
+{!! $errors->first('Correo','<div class="invalid-feedback">:message</div>') !!}
 <br>
 
 <label for="Edad">{{ 'Edad' }}</label>
-<input type="text" name="Edad" id="Edad"
-value="{{ isset($empleado->Edad)?$empleado->Edad : '' }}">
+<input type="text" name="Edad" id="Edad" class="{{ $errors->has('Edad')?'is-invalid':'' }}"
+value="{{ isset($empleado->Edad)?$empleado->Edad : old('Edad') }}">
+{!! $errors->first('Edad','<div class="invalid-feedback">:message</div>') !!}
 <br>
 
 <input type="submit" value="{{ $Modo =='crear' ?  'Agregar' : 'Modificar'}}">
