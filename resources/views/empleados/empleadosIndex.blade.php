6eb708cabdfb9a33a5d9fe40bb0aef5f0fa2258c
Amyly10 <amy.flores@alumnos.udg.mx>
2020-12-06 13:43:42 -0600

    prueba de push con estilo de empleados

--- resources/views/empleados/empleadosIndex.blade.php
+++ resources/views/empleados/empleadosIndex.blade.php
@@ -14,33 +14,35 @@
 
 <a href="{{ url('empleados/create') }}">Agregar empleado</a>
 
-<table class="table table-light">
-    <thead class="thead-light">
-        <tr>
-            <th>N°</th>
-            <th>Nombre</th>
-            <th>Apellido Paterno</th>
-            <th>Apellido Materno</th>
-            <th>Direccion</th>
-            <th>Municipio</th>
-            <th>Telefono</th>
-            <th>Correo</th>
-            <th>Acciones</th>
+<div class="container">
+<h1 class="mb-8"> Empleados registrados :) </h1>
+<table class="text-left w-full">
+    <thead class="bg-black flex text-white w-full">
+        <tr class="flex w-full mb-4">
+            <th class="p-4 w-1/4">N°</th>
+            <th class="p-4 w-1/4">Nombre</th>
+            <th class="p-4 w-1/4">Apellido Paterno</th>
+            <th class="p-4 w-1/4">Apellido Materno</th>
+            <th class="p-4 w-1/4">Direccion</th>
+            <th class="p-4 w-1/4">Municipio</th>
+            <th class="p-4 w-1/4">Telefono</th>
+            <th class="p-4 w-1/4">Correo</th>
+            <th class="p-4 w-1/4">Acciones</th>
         </tr>
     </thead>
-    <tbody>
+    <tbody class="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full" style="height: 50vh;">
 
         @foreach ($empleados as $empleado)
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $empleado->Nombre }}</td>
-                <td>{{ $empleado->ApellidoPaterno }}</td>
-                <td>{{ $empleado->ApellidoMaterno }}</td>
-                <td>{{ $empleado->Direccion }}</td>
-                <td>{{ $empleado->Municipio }}</td>
-                <td>{{ $empleado->Telefono }}</td>
-                <td>{{ $empleado->Correo }}</td>
-                <td>
+            <tr class="flex w-full mb-4">
+                <td class="p-4 w-1/4">{{ $loop->iteration }}</td>
+                <td class="p-4 w-1/4">{{ $empleado->Nombre }}</td>
+                <td class="p-4 w-1/4">{{ $empleado->ApellidoPaterno }}</td>
+                <td class="p-4 w-1/4">{{ $empleado->ApellidoMaterno }}</td>
+                <td class="p-4 w-1/4">{{ $empleado->Direccion }}</td>
+                <td class="p-4 w-1/4">{{ $empleado->Municipio }}</td>
+                <td class="p-4 w-1/4">{{ $empleado->Telefono }}</td>
+                <td class="p-4 w-1/4">{{ $empleado->Correo }}</td>
+                <td class="p-4 w-1/4">
                     <a href="{{ url('/empleados/'.$empleado->id.'/edit') }}">
                         Editar
                     </a>
@@ -61,6 +63,7 @@
 
     </tbody>
 </table>
+</div>
 
 {{ $empleados->links() }}
 
